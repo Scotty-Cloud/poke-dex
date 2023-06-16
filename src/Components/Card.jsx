e31e436d0ed2f70786a41005a4f1ecd228f65384
scotty <scottadamr01@gmail.com>
2023-06-16 19:31:45 -0400

    add styling to card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,7 +5,8 @@ const Card = () => {
         <>
             <div className="card">
                 <h2>1</h2>
-                <img src="" alt="" />
+                <img src="./images/charmander.png" alt="" />
+                <h2>Charmander</h2>
             </div>
         </>    
     );
