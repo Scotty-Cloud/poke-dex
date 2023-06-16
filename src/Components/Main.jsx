e2afc62f2d7df5cef076e7b52346ce96c0c51973
scotty <scottadamr01@gmail.com>
2023-06-15 22:05:22 -0400

    update main file

---
+++ src/Components/Main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Main = () => {
+    return(
+        <>
+            <div className="container">
+                <div className="left-content">
+
+                </div>
+                <div className="right-container">
+                    
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Main
